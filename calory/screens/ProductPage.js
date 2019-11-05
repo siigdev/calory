49acf88a777c0235dff88459c423796d2df39b7f
siigdev <sebbahlol@gmail.com>
2019-11-05 21:33:31 +0100

    Fixed view bug

--- calory/screens/ProductPage.js
+++ calory/screens/ProductPage.js
@@ -25,14 +25,9 @@ export default class ProductPage extends Component {
 
   render() {
     return (
-      <View
-        style={{
-          flex: 1,
-          flexDirection: 'column',
-          justifyContent: 'flex-end',
-        }}>
+      <Block>
         <Text>Hej :)</Text>
-      </View>
+      </Block>
     )
   }
 }
