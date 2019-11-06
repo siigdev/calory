c9e164182ac1bf9cbf6b3f5ea0de4e244be3d861
siigdev <sebbahlol@gmail.com>
2019-11-06 18:22:16 +0100

    Switch works with theme

--- calory/components/Switch.js
+++ calory/components/Switch.js
@@ -1,18 +1,23 @@
 import React, { Component } from 'react'
 import { Switch } from 'react-native'
 import { theme } from '../constants';
+import { AppConsumer } from '../AppContextProvider'
 
 export default class SwitchInput extends Component {
     render() {
         const { value, ...props } = this.props;
         let thumbColor = null;
         return (
+            <AppConsumer>
+        {appConsumer => (
             <Switch
             thumbColor={thumbColor}
-            ios_backgroundColor={theme.colors.gray}
-            trackColor={{true: theme.colors.primary}}
+            ios_backgroundColor={appConsumer.theme.colors.gray}
+            trackColor={{true: appConsumer.theme.colors.primary}}
             value={value}
             {...props}/>
+            )}
+            </AppConsumer>
         )
     }
 }
