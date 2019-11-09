ffa6f4bba24495b518faab9b5d6ad996b07cf16f
siigdev <sebbahlol@gmail.com>
2019-11-09 11:13:52 +0100

    Initial elements

--- calory/screens/Friends.js
+++ calory/screens/Friends.js
@@ -1,12 +1,23 @@
 import React, { Component } from 'react';
-import { Text } from '../components'
+import { Text, Block, Card, Button } from '../components'
 import { ScrollView } from 'react-native';
+import { theme } from '../constants';
 
-export default class Friends extends Component{
+export default class Friends extends Component {
     render() {
         return (
-            <ScrollView showsVerticalScrollIndicator={false}>
-                <Text>Here comes Friends</Text>
+            <ScrollView showsVerticalScrollIndicator={false} style={{ backgroundColor: theme.colors.gray4 }}>
+                <Block padding={[0, theme.sizes.base]}>
+                <Button gradient onPress={() => this.addToHistory()}>
+              <Text bold white center>Add new friend</Text>
+            </Button>
+            <Block style={{ marginBottom: theme.sizes.base / 2 }}>
+            <Text spacing={0.4} transform="uppercase">Recent friend activity</Text>
+          </Block>
+                    <Card><Text>You friend did this :) </Text></Card>
+                    <Card><Text>You friend did this :) </Text></Card>
+                    <Card><Text>You friend did this :) </Text></Card>
+                </Block>
             </ScrollView>
         )
     }
