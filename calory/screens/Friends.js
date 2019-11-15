cef676b2c82535b5abb41a42a992c30bf08917f4
siigdev <sebbahlol@gmail.com>
2019-11-15 20:07:29 +0100

    Method for checking if added friend exists

--- calory/screens/Friends.js
+++ calory/screens/Friends.js
@@ -1,19 +1,44 @@
 import React, { Component } from 'react';
-import { Text, Block, Card, Button } from '../components'
-import { ScrollView } from 'react-native';
+import { Text, Block, Card, Button, Input } from '../components'
+import { ScrollView, StyleSheet } from 'react-native';
 import { theme } from '../constants';
+import firebase from 'firebase';
 
 export default class Friends extends Component {
+    state = {
+        friendEmail: ''
+    }
+    addFriend() {
+
+
+        //const currUser = firebase.auth().currentUser.uid;
+        firebase.auth().fetchSignInMethodsForEmail(this.state.friendEmail)
+        .then(result => {
+            if (result.length === 0){
+                console.warn('nono')
+            }
+            else {
+                console.warn('yesyes')
+            }
+        })
+    }
     render() {
         return (
             <ScrollView showsVerticalScrollIndicator={false} style={{ backgroundColor: theme.colors.gray4 }}>
                 <Block padding={[0, theme.sizes.base]}>
-                <Button gradient onPress={() => this.addToHistory()}>
-              <Text bold white center>Add new friend</Text>
-            </Button>
-            <Block style={{ marginBottom: theme.sizes.base / 2 }}>
-            <Text spacing={0.4} transform="uppercase">Recent friend activity</Text>
-          </Block>
+
+                    <Input
+                        label="Email"
+                        style={[styles.input]}
+                        defaultValue={this.state.friendEmail}
+                        onChangeText={text => this.setState({ friendEmail: text })}
+                    />
+                    <Button gradient onPress={() => this.addFriend()}>
+                        <Text bold white center>Add new friend</Text>
+                    </Button>
+                    <Block style={{ marginBottom: theme.sizes.base / 2 }}>
+                        <Text spacing={0.4} transform="uppercase">Recent friend activity</Text>
+                    </Block>
                     <Card><Text>You friend did this :) </Text></Card>
                     <Card><Text>You friend did this :) </Text></Card>
                     <Card><Text>You friend did this :) </Text></Card>
@@ -22,3 +47,11 @@ export default class Friends extends Component {
         )
     }
 }
+const styles = StyleSheet.create({
+    input: {
+        borderRadius: 0,
+        borderWidth: 0,
+        borderBottomColor: theme.colors.gray,
+        borderBottomWidth: 1
+    }
+})  
