3b71d5ad95874979e83dfa233c7b2b6464b1219c
siigdev <sebbahlol@gmail.com>
2019-11-15 22:33:58 +0100

    Updated the forgot page styling

--- calory/screens/ForgotPassword.js
+++ calory/screens/ForgotPassword.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { StyleSheet, Image } from 'react-native';
+import { StyleSheet, Image, KeyboardAvoidingView } from 'react-native';
 import { theme } from '../constants';
 import { Text, Button, Input, Block } from '../components';
 import firebase from 'firebase';
@@ -14,39 +14,35 @@ export default class ForgotPassword extends Component {
             console.warn("SENT")
         }).catch((e) => console.warn(e))
     };
+
+
+
     render() {
-        const { loading } = this.state;
         return (
-            <Block animation="zoomIn" duration={400}  padding={[0, theme.sizes.base * 2]}>
-                <Block middle>
-                    <Block center bottom flex={0.2}>
-                        <Image
-                            style={{ width: 150, height: 150 }}
-                            source={require('../assets/icons/forgot-password.png')}
-                        />
+            <KeyboardAvoidingView style={styles.forgot} behavior="padding">
+                <Block middle animation="zoomIn" duration={400} padding={[0, theme.sizes.base*2]}>
                         <Text h2 center bold>Forgot your password?</Text>
-                        <Text h4 center gray2>Enter your email for further instructions</Text>
-                    </Block>
-                    <Block middle flex={0.3} margin={[0, theme.sizes.padding * 2]}>
-                        <Input
-                            label="Email"
-                            style={[styles.input]}
-                            defaultValue={this.state.email}
-                            onChangeText={text => this.setState({ email: text })}
-                        />
-                        <Button gradient onPress={() => this.handleForgot()}>
-                            {loading ?
-                                <ActivityIndicator size="small" color="white" /> :
-                                <Text bold white center>Login</Text>
-                            }
-                        </Button>
-                    </Block>
-                </Block>
-            </Block>
+                        <Text h4 center gray2 style={{paddingBottom: theme.sizes.base*2}}>Enter your email for further instructions</Text>
+                    <Input
+                        label="Email"
+                        style={[styles.input]}
+                        defaultValue={this.state.email}
+                        onChangeText={text => this.setState({ email: text })}
+                    />
+                    <Button gradient onPress={() => this.handleForgot()}>
+                        <Text bold white center>Login</Text>
+                    </Button>
+                </Block >
+            </KeyboardAvoidingView >
         )
     }
 }
+
 const styles = StyleSheet.create({
+    forgot: {
+        flex: 1,
+        justifyContent: 'center',
+    },
     input: {
         borderRadius: 0,
         borderWidth: 0,
