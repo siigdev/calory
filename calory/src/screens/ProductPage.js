3ff702e8cdd3a112dac58af2ef6c0629e527a15a
siigdev <sebbahlol@gmail.com>
2019-12-05 19:58:51 +0100

    Update product page

--- calory/src/screens/ProductPage.js
+++ calory/src/screens/ProductPage.js
@@ -1,8 +1,10 @@
 import React, { Component } from 'react'
-import { Text, Block, Input, Progress, Divider, Button } from '../components'
+import { Text, Block, Input, Progress, Divider, Button, Card } from '../components'
 import ApiKeys from '../constants/ApiKeys'
-import { StyleSheet, ScrollView, Picker, Image } from 'react-native'
+import { LinearGradient } from 'expo-linear-gradient';
+import { StyleSheet, ScrollView, Picker, Image, View } from 'react-native'
 import firebase from 'firebase';
+import { AppConsumer } from '../AppContextProvider'
 
 import { theme } from '../constants';
 
@@ -44,111 +46,122 @@ export default class ProductPage extends Component {
     const currUser = firebase.auth().currentUser.uid;
 
     firebase.database().ref('calories/').child(currUser).push({
-      amount: this.state.calories*this.state.grams / 100,
+      amount: this.state.calories * this.state.grams / 100,
       date: firebase.database.ServerValue.TIMESTAMP
     })
   }
   calculateWorkoutTime() {
     if (this.state.gender == 'Male') {
-      BMR = (13.397*this.state.weight)+(4.799*this.state.height)-(5.677*this.state.age)+88.362
+      BMR = (13.397 * this.state.weight) + (4.799 * this.state.height) - (5.677 * this.state.age) + 88.362
     }
     else {
-      BMR = (9.247*this.state.weight)+(3.098*this.height)+(4.330*this.state.age)+447.593
+      BMR = (9.247 * this.state.weight) + (3.098 * this.height) + (4.330 * this.state.age) + 447.593
     }
-    this.setState({calorieburn: BMR})
+    this.setState({ calorieburn: BMR })
   }
   render() {
     return (
-      <ScrollView showsVerticalScrollIndicator={false}>
-        <Block padding={[0, theme.sizes.base * 2]}>
-          <Text h1 center bold style={{ marginTop: 10 }}>{this.state.name} </Text>
-          <Text h3 center primary>{this.state.calories} kcal / 100 g</Text>
-          <Block row style={styles.container}>
-            <Block center flex={1}>
-              <Input
-                style={[styles.input]}
-                textAlign={'center'}
-                keyboardType='phone-pad'
-                defaultValue={this.state.grams}
-                onChangeText={text => {this.setState({ grams: text }), this.calculateWorkoutTime()}}
-              />
-            </Block>
-            <Block center flex={1}>
-              <Picker
-                selectedValue={this.state.language}
-                style={[styles.picker]}
-                onValueChange={(itemValue, itemIndex) =>
-                  this.setState({ language: itemValue })
-                }>
-                <Picker.Item label="Grams" value="grams" />
-                <Picker.Item label="Pieces" value="pieces" />
-              </Picker>
-            </Block>
-          </Block>
-
-
-          <Block row>
-            <Block center flex={1}>
-            <Text size={20} spacing={1} primary>{Math.floor(((this.state.calorieburn/24)/10.0)*0.6*10)} MIN</Text>
-              <Text spacing={0.7}>Løb</Text>
-            </Block>
-
-            <Block center flex={1}>
-              <Text size={20} spacing={1} primary>{Math.floor(((this.state.calorieburn/24)/8.3)*0.6*10)} MIN</Text>
-              <Text spacing={0.7}>Svømning</Text>
-            </Block>
-
-            <Block center flex={1}>
-              <Text size={20} spacing={1} primary>{Math.floor(((this.state.calorieburn/24)/7.5)*0.6*10)} MIN</Text>
-              <Text spacing={0.7}>Cykling</Text>
-            </Block>
-          </Block>
-
-          <Divider />
-
-          <Block >
-            <Block style={{ marginBottom: theme.sizes.base }}>
-              <Block row space="between" style={{ paddingLeft: 6 }}>
-                <Text body spacing={0.7}>Protein</Text>
-                <Text caption spacing={0.7}>55%</Text>
+      <AppConsumer>
+        {appConsumer => (
+          <ScrollView style={{ backgroundColor: theme.colors.gray4 }} showsVerticalScrollIndicator={false}>
+            <LinearGradient colors={[appConsumer.theme.colors.primary, appConsumer.theme.colors.secondary]} start={{ x: 0, y: 0 }} end={{ x: 1, y: 1 }} style={styles.linearGradient}>
+              <Block padding={[0, theme.sizes.base * 2]}>
+                <Text h1 center white bold style={{ marginTop: 10 }}>{this.state.name} </Text>
+                <Text h3 center white>{this.state.calories} kcal / 100 g</Text>
               </Block>
-              <Progress value={0.55} />
-            </Block>
 
-            <Block style={{ marginBottom: theme.sizes.base }}>
-              <Block row space="between" style={{ paddingLeft: 6 }}>
-                <Text body spacing={0.7}>Carbohydrates</Text>
-                <Text caption spacing={0.7}>35%</Text>
+              <Block row style={styles.container}>
+                <Block center flex={1}>
+                  <Input
+                    style={[styles.input]}
+                    textAlign={'center'}
+                    keyboardType='phone-pad'
+                    defaultValue={this.state.grams}
+                    onChangeText={text => { this.setState({ grams: text }), this.calculateWorkoutTime() }}
+                  />
+                </Block>
+                <Block center flex={1}>
+                  <View style={[styles.picker]}>
+                    <Picker
+                      style={[styles.picker]}
+                      selectedValue={this.state.language}
+                      onValueChange={(itemValue, itemIndex) =>
+                        this.setState({ language: itemValue })
+                      }>
+                      <Picker.Item label="Grams" value="grams" />
+                      <Picker.Item label="Pieces" value="pieces" />
+                    </Picker>
+                  </View>
+                </Block>
               </Block>
-              <Progress value={0.35} />
+            </LinearGradient>
+            <Block padding={[0, theme.sizes.base]}>
+              <Card style={{ padding: theme.sizes.base / 2, marginTop: theme.sizes.base }}>
+                <Block row>
+                  <Block center flex={1}>
+                    <Text size={20} spacing={1} style={{color: appConsumer.theme.colors.primary}}>{Math.floor(((this.state.calorieburn / 24) / 10.0) * 0.6 * 10)} MIN</Text>
+                    <Text spacing={0.7}>Løb</Text>
+                  </Block>
+
+                  <Block center flex={1}>
+                    <Text size={20} spacing={1} style={{color: appConsumer.theme.colors.primary}}>{Math.floor(((this.state.calorieburn / 24) / 8.3) * 0.6 * 10)} MIN</Text>
+                    <Text spacing={0.7}>Svømning</Text>
+                  </Block>
+
+                  <Block center flex={1}>
+                    <Text size={20} spacing={1} style={{color: appConsumer.theme.colors.primary}}>{Math.floor(((this.state.calorieburn / 24) / 7.5) * 0.6 * 10)} MIN</Text>
+                    <Text spacing={0.7}>Cykling</Text>
+                  </Block>
+                </Block>
+              </Card>
+
+              <Card style={{ padding: theme.sizes.base / 2 }}>
+                <Block >
+                  <Block style={{ marginBottom: theme.sizes.base }}>
+                    <Block row space="between" style={{ paddingLeft: 6 }}>
+                      <Text body spacing={0.7}>Protein</Text>
+                      <Text caption spacing={0.7}>55%</Text>
+                    </Block>
+                    <Progress value={0.55} />
+                  </Block>
+
+                  <Block style={{ marginBottom: theme.sizes.base }}>
+                    <Block row space="between" style={{ paddingLeft: 6 }}>
+                      <Text body spacing={0.7}>Carbohydrates</Text>
+                      <Text caption spacing={0.7}>35%</Text>
+                    </Block>
+                    <Progress value={0.35} />
+                  </Block>
+
+                  <Block style={{ marginBottom: theme.sizes.base }}>
+                    <Block row space="between" style={{ paddingLeft: 6 }}>
+                      <Text body spacing={0.7}>Fat</Text>
+                      <Text caption spacing={0.7}>10%</Text>
+                    </Block>
+                    <Progress value={0.10} />
+                  </Block>
+
+                  <Divider margin={0} />
+
+                  <Block row center space="between">
+                    <Text>Calories in total</Text>
+                    <Text size={20} spacing={1} style={{color: appConsumer.theme.colors.primary}}>{this.state.calories * this.state.grams / 100} kcal.</Text>
+                  </Block>
+
+                  <Button gradient onPress={() => this.addToHistory()}>
+                    <Text bold white center>Add to history</Text>
+                  </Button>
+
+                  <Image
+                    style={{ width: 65, height: 25, alignSelf: 'flex-end' }}
+                    source={require('../assets/images/edamambadge.png')}
+                  />
+                </Block>
+              </Card>
             </Block>
-
-            <Block style={{ marginBottom: theme.sizes.base }}>
-              <Block row space="between" style={{ paddingLeft: 6 }}>
-                <Text body spacing={0.7}>Fat</Text>
-                <Text caption spacing={0.7}>10%</Text>
-              </Block>
-              <Progress value={0.10} />
-            </Block>
-
-            <Divider margin={0} />
-
-            <Block row center space="between">
-              <Text>Calories in total</Text>
-              <Text size={20} spacing={1} primary>{this.state.calories * this.state.grams / 100} kcal.</Text>
-            </Block>
-
-            <Button gradient onPress={() => this.addToHistory()}>
-              <Text bold white center>Add to history</Text>
-            </Button>
-
-            <Image
-          style={{width: 65, height: 25, alignSelf: 'flex-end'}}
-          source={require('../assets/images/edamambadge.png')}
-        />
-          </Block>
-        </Block>
-      </ScrollView >
+          </ScrollView >
+        )}
+      </AppConsumer>
     )
   }
 }
@@ -161,9 +174,9 @@ const styles = StyleSheet.create({
   },
   input: {
     height: 40,
-    width: 100,
+    backgroundColor: theme.colors.white,
+    width: 150,
     borderRadius: 3,
-    borderWidth: 0,
     borderColor: theme.colors.gray,
     borderWidth: 1
   },
@@ -171,11 +184,16 @@ const styles = StyleSheet.create({
     alignContent: 'center',
     textAlign: 'center',
     alignItems: 'center',
-    height: 25,
-    width: 130,
+    backgroundColor: theme.colors.white,
+    height: 40,
+    width: 150,
     borderRadius: 3,
-    borderWidth: 0,
     borderColor: theme.colors.gray,
-    borderWidth: 1
-  }
+    borderWidth: 1,
+    overflow: 'hidden'
+  },
+  linearGradient: {
+    height: theme.sizes.base * 11,
+    flex: 1,
+  },
 })  
